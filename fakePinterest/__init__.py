80b3ba2ba667c8f5906fa4f5818e9b86b08d261b
Felipe Barbosa <fpbarbosa@live.com>
2024-12-30 21:54:10 -0300

    mudança database

--- fakePinterest/__init__.py
+++ fakePinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"]= os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "dc107b82d1cb720a18c7f98f1db45619"   #Parâmetro para a criptografia da senha utilizado pelo Bcrypt
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts" #Caminho dpara a pasta de armazamento das fotos
 
